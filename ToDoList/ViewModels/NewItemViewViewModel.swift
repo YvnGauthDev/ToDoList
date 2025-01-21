e96074b143676345de4355ada1f7341f163e0d5b
Yvan Gauthier <ygauthier25@icloud.com>
2025-01-21 13:09:42 +0100

    NewItem and ToDoListItem

--- ToDoList/ViewModels/NewItemViewViewModel.swift
+++ ToDoList/ViewModels/NewItemViewViewModel.swift
@@ -5,18 +5,45 @@
 //  Created by Yvan Gauthier on 12/01/2025.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class NewItemViewViewModel: ObservableObject {
     
     @Published var title = ""
     @Published var dueDate = Date()
-    @Published var showAlert = false 
+    @Published var showAlert = false
     
     init() {}
     
     func save() {
-        // code 
+        guard canSave else {
+            return
+        }
+        // Get the current user id
+        guard let uId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        // Create model
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(
+            id: newId,
+            title: title,
+            dueDate: dueDate.timeIntervalSince1970,
+            createdDate: Date().timeIntervalSince1970,
+            isDone: false)
+        
+        // Save model
+        let db = Firestore.firestore()
+        db.collection("user")
+            .document(uId)
+            .collection("todos")
+            .document(newId)
+            .setData(newItem.asDictionary())
+        
+        
     }
     var canSave: Bool{
         guard !title.trimmingCharacters(in: .whitespaces).isEmpty else {
